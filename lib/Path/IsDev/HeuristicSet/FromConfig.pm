c36891d01f53e8d852222fe1f3a487d6f2431c94
Kent Fredric <kentfredric@gmail.com>
2013-10-07 03:36:35 +1300

    Build results of 95c80cb (on master)

--- lib/Path/IsDev/HeuristicSet/FromConfig.pm
+++ lib/Path/IsDev/HeuristicSet/FromConfig.pm
@@ -55,7 +55,7 @@ By default, it will try to read a configuration file in one of the following pat
     $HOME/.local/share/.path_isdev_heuristicset_fromconfig/config.json
     $HOME/.path_isdev_heuristicset_fromconfig/config.json
 
-Which ever one it is unfortunatley dependent on the sort of mood File::HomeDir is in, and wether or not
+Which ever one it is unfortunately dependent on the sort of mood File::HomeDir is in, and whether or not
 you have C<xdg-user-dir> in your C<$PATH>
 
 Either way, if such a path does not exist the first time you use this module, it will be created
@@ -63,9 +63,9 @@ for you from the default template in the distributions share directory.
 
 Edit it to your liking.
 
-If you mess it up, just delete it, run the code  again, and bam, its back! :D
+If you mess it up, just delete it, run the code  again, and its back! :D
 
-In fact, its so agressive at this, I had to put a bit of code in the tests to stop it
+In fact, its so aggressive at this, I had to put a bit of code in the tests to stop it
 creating those directories during tests >_>.
 
 Pester File::UserConfig if you want this logic improved.
