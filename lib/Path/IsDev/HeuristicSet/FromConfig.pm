72801da6adc5925333a442bbcfb9e0b34d9e0239
Kent Fredric <kentfredric@gmail.com>
2013-10-18 23:49:52 +1300

    Build results of 3ac1d24 (on master)

--- lib/Path/IsDev/HeuristicSet/FromConfig.pm
+++ lib/Path/IsDev/HeuristicSet/FromConfig.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Path::IsDev::HeuristicSet::FromConfig::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Path::IsDev::HeuristicSet::FromConfig::VERSION = '0.002000';
+  $Path::IsDev::HeuristicSet::FromConfig::VERSION = '0.001001';
 }
 
 # ABSTRACT: A Custom Heuristic Set from a configuration file
@@ -45,7 +45,7 @@ Path::IsDev::HeuristicSet::FromConfig - A Custom Heuristic Set from a configurat
 
 =head1 VERSION
 
-version 0.002000
+version 0.001001
 
 =head1 SYNOPSIS
 
